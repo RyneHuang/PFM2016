b9950d498d9f32df88f5de6520c5cf88569d960d
黄河 <hhsky0618@hotmail.com>
2016-04-17 23:23:38 +0800

    初始化，验证单元测试

---
+++ PFM/PFMConfigurationsTests/Class1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PFMConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFMConfigurations.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void addTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
